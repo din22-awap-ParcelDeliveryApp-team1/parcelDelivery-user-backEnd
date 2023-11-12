b3fd62cef793484055b24423e62c15b4c63b18d5
liisatormakangas <liisa.tormakangas@gmail.fi>
2023-11-12 13:06:31 +0200

    fetch parcel history data resolves #5

--- dataBase.js
+++ dataBase.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-//Create database connection to mysql
 const mysql2_1 = __importDefault(require("mysql2"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+//Create database connection to mysql
 const connection = mysql2_1.default.createConnection({
     host: process.env.DB_HOST,
     user: "root",

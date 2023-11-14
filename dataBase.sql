b874a64a858054a0c7e7363a9c9b2212bdf3d62a
Huyen Nguyen <c2ngth00@students.oamk.fi>
2023-11-14 22:45:25 +0200

    modified database

--- dataBase.sql
+++ dataBase.sql
@@ -1,70 +1,17 @@
-drop database if exists parcelDelivery;
-create database parcelDelivery;
-use parcelDelivery;
 
-CREATE TABLE `user` (
-  `id_user` INT NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `first_name` VARCHAR(45) NOT NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  `telephone` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `street_address` VARCHAR(45) NOT NULL,
-  `postal_code` CHAR(5) NOT NULL,
-  `city` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `parcel` (
-  `id_parcel` INT NOT NULL AUTO_INCREMENT,
-  `id_user` INT DEFAULT NULL,
-  `locker_number` TINYINT DEFAULT NULL,
-  `reciever_name` VARCHAR(45) NOT NULL,
-  `reciever_telephone` VARCHAR(45) NOT NULL,
-  `reciever_street_address` VARCHAR(45) NOT NULL,
-  `reciever_postal_code` CHAR(6) NOT NULL,
-  `reciever_city` VARCHAR(45) NOT NULL,
-  `sender_name` VARCHAR(45) NOT NULL,
-  `sender_telephone` VARCHAR(45) DEFAULT NULL,
-  `sender_street_address` VARCHAR(45) DEFAULT NULL,
-  `sender_postal_code` CHAR(5) DEFAULT NULL,
-  `sender_city` VARCHAR(45) DEFAULT NULL,
-  `parcel_dropoff_date` DATE NOT NULL,
-  `parcel_pickup_date` DATE NOT NULL,
-  `parcel_last_pickup_date` DATE NOT NULL,
-  `parcel_dropoff_code` INT DEFAULT NULL,
-  `parcel_pickup_code` INT DEFAULT NULL,
-  `status` TINYINT NOT NULL,
-  PRIMARY KEY (`id_parcel`),
-  KEY `locker_number_idx` (`locker_number`),
-  KEY `id_user_idx` (`id_user`),
-  CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*
-Values for parcel status:
-1 - parcel is in the dropoff locker
-2 - parcel is transported (by consumer user to dropoff locker or by driver to pickup locker)
-3 - parcel is in the pickup locker
-4 - parcel is delivered to the reciever (final status)
+/*ALTER TABLE `parcel`
+ADD CONSTRAINT `locker_number` FOREIGN KEY (`locker_number`) REFERENCES `locker` (`locker_number`) ON DELETE RESTRICT;
+
+INSERT INTO `user` (`user_name`, `password`, `first_name`, `last_name`, `telephone`, `email`, `street_address`, `postal_code`, `city`) VALUES ('Huyen', 'Huyen2023+', 'Huyen', 'Nguyen', '+358123456789', 'huyen@gmail.com', 'Rantatie 5', '90802', 'Oulu');
+INSERT INTO `user` (`user_name`, `password`, `first_name`, `last_name`, `telephone`, `email`, `street_address`, `postal_code`, `city`) VALUES ('Anne', 'Anne2023+', 'Anne', 'Kauppinen', '+358023456789', 'anne@gmail.com', 'Rantatie 9', '90801', 'Oulu');	
+INSERT INTO `user` (`user_name`, `password`, `first_name`, `last_name`, `telephone`, `email`, `street_address`, `postal_code`, `city`) VALUES ('Matti', 'Matti2023+', 'Matti', 'Meikäläinen', '+358123456798', 'matti@gmail.com', 'Rantatie 7', '90803', 'Oulu');
+
+INSERT INTO `parcel` (`id_user`, `locker_number`, `reciever_name`, `reciever_telephone`, `reciever_street_address`, `reciever_postal_code`, `reciever_city`, `sender_name`, `sender_telephone`, `sender_street_address`, `sender_postal_code`, `sender_city`, `parcel_dropoff_date`, `parcel_pickup_date`, `parcel_last_pickup_date`, `parcel_dropoff_code`, `parcel_pickup_code`, `status`) VALUES ('1', '1', 'Anne Kauppinen', '+358023456789', 'Rantatie 9', '90801', 'Oulu', 'Matti Meikäläinen', '+358123456798', 'Rantatie 7', '90803', 'Oulu', '2021-05-01', '2021-05-02', '2021-05-03', '123456', '654321', '4');
+INSERT INTO `parcel` (`id_user`, `locker_number`, `reciever_name`, `reciever_telephone`, `reciever_street_address`, `reciever_postal_code`, `reciever_city`, `sender_name`, `sender_telephone`, `sender_street_address`, `sender_postal_code`, `sender_city`, `parcel_dropoff_date`, `parcel_pickup_date`, `parcel_last_pickup_date`, `parcel_dropoff_code`, `parcel_pickup_code`, `status`) VALUES ('2', '2', 'Matti Meikäläinen', '+358123456798', 'Rantatie 7', '90803', 'Oulu', 'Anne Kauppinen', '+358023456789', 'Rantatie 9', '90801', 'Oulu', '2021-05-01', '2021-05-02', '2021-05-03', '123456', '654321', '4');
+INSERT INTO `parcel` (`id_user`, `locker_number`, `reciever_name`, `reciever_telephone`, `reciever_street_address`, `reciever_postal_code`, `reciever_city`, `sender_name`, `sender_telephone`, `sender_street_address`, `sender_postal_code`, `sender_city`, `parcel_dropoff_date`, `parcel_pickup_date`, `parcel_last_pickup_date`, `parcel_dropoff_code`, `parcel_pickup_code`, `status`) VALUES ('3', '3', 'Huyen Nguyen', '+358123456789', 'Rantatie 5', '90802', 'Oulu', 'Anne Kauppinen', '+358023456789', 'Rantatie 9', '90801', 'Oulu', '2021-05-01', '2021-05-02', '2021-05-03', '123456', '654321', '4');
+
+INSERT INTO `locker` (`parcel_id`, `locker_number`, `cabinet_status`) VALUES ('1', '1', '4');
+INSERT INTO `locker` (`parcel_id`, `locker_number`, `cabinet_status`) VALUES ('2', '2', '4');
+INSERT INTO `locker` (`parcel_id`, `locker_number`, `cabinet_status`) VALUES ('3', '3', '4');
 */
-
-CREATE TABLE `locker` (
-  `id_cabinet` TINYINT NOT NULL,
-  `parcel_id` INT DEFAULT NULL,
-  `locker_number` TINYINT NOT NULL,
-  `cabinet_status` TINYINT NOT NULL,
-  PRIMARY KEY (`id_cabinet`),
-  UNIQUE KEY `unique_locker_number` (`locker_number`),
-  KEY `parcel_id_idx` (`parcel_id`),
-  CONSTRAINT `parcel_id` FOREIGN KEY (`parcel_id`) REFERENCES `parcel` (`id_parcel`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*
-Values for cabinet status:
-1 - cabinet is empty
-2 - parcel to dropoff
-3 - parcel to pickup
-*/
-
-ALTER TABLE `parcel`
-ADD CONSTRAINT `locker_number` FOREIGN KEY (`locker_number`) REFERENCES `locker` (`locker_number`) ON DELETE SET NULL;

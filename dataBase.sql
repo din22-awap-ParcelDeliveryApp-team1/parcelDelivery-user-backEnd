344c5eb1382acaddc08a852cb25db19052488738
Huyen Nguyen <c2ngth00@students.oamk.fi>
2023-11-16 12:04:08 +0200

    changed database query

--- dataBase.sql
+++ dataBase.sql
@@ -34,13 +34,7 @@ CREATE TABLE `parcel` (
   `parcel_last_pickup_date` DATE NOT NULL,
   `parcel_dropoff_code` INT NOT NULL,
   `parcel_pickup_code` INT NOT NULL,
-  `parcel_status` TINYINT NOT NULL,
-  `parcel_dropoff_locker` TINYINT(5) NOT NULL,
-  `parcel_pickup_locker` TINYINT(5) NOT NULL,
-  `parcel_height` FLOAT NOT NULL,
-  `parcel_width` FLOAT NOT NULL,
-  `parcel_depth` FLOAT NOT NULL,
-  `parcel_mass` FLOAT NOT NULL,
+  `status` TINYINT NOT NULL,
   PRIMARY KEY (`id_parcel`),
   KEY `id_user_idx` (`id_user`),
   CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE SET NULL
@@ -56,7 +50,13 @@ CREATE TABLE `locker` (
   CONSTRAINT `parcel_id` FOREIGN KEY (`parcel_id`) REFERENCES `parcel` (`id_parcel`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
+ALTER TABLE `parcelDelivery`.`parcel` 
+ADD COLUMN `parcel_dropoff_locker` TINYINT(5) NOT NULL AFTER `status`,
+ADD COLUMN `parcel_pickup_locker` TINYINT(5) NOT NULL AFTER `parcel_dropoff_locker`,
+ADD COLUMN `parcel_height` FLOAT NOT NULL AFTER `parcel_pickup_locker`,
+ADD COLUMN `parcel_width` FLOAT NOT NULL AFTER `parcel_height`,
+ADD COLUMN `parcel_depth` FLOAT NOT NULL AFTER `parcel_width`,
+ADD COLUMN `marcel_mass` FLOAT NOT NULL AFTER `parcel_depth`;
 /*
 Values for cabinet status:
 1 - cabinet is empty
@@ -75,49 +75,49 @@ Values for parcel status:
 
 -- insert values:
 -- Insert user data
-INSERT INTO `user` 
-  (`user_name`, `password`, `first_name`, `last_name`, `telephone`, `email`, `street_address`, `postal_code`, `city`)
-VALUES
-  ('john_doe', 'password123', 'John', 'Doe', '123456789', 'john.doe@example.com', '123 Main St', '12345', 'City1'),
-  ('jane_smith', 'pass456', 'Jane', 'Smith', '987654321', 'jane.smith@example.com', '456 Oak St', '54321', 'City2'),
-  ('ora_smith', 'pass856', 'Ora', 'Silva', '987654321', 'or.silvah@example.com', '45 west St', '5643', 'City4');
+-- INSERT INTO `user` 
+--   (`user_name`, `password`, `first_name`, `last_name`, `telephone`, `email`, `street_address`, `postal_code`, `city`)
+-- VALUES
+--   ('john_doe', 'password123', 'John', 'Doe', '123456789', 'john.doe@example.com', '123 Main St', '12345', 'City1'),
+--   ('jane_smith', 'pass456', 'Jane', 'Smith', '987654321', 'jane.smith@example.com', '456 Oak St', '54321', 'City2'),
+--   ('ora_smith', 'pass856', 'Ora', 'Silva', '987654321', 'or.silvah@example.com', '45 west St', '5643', 'City4');
   
 
--- Insert parcel data
-INSERT INTO `parcel`
-  (`id_user`, `reciever_name`, `reciever_telephone`, `reciever_street_address`, `reciever_postal_code`, `reciever_city`,
-   `sender_name`, `sender_telephone`, `sender_street_address`, `sender_postal_code`, `sender_city`,
-   `parcel_dropoff_date`, `parcel_pickup_date`, `parcel_last_pickup_date`,
-   `parcel_dropoff_code`, `parcel_pickup_code`, `parcel_status`, `parcel_dropoff_locker`, `parcel_pickup_locker`,
-   `parcel_height`, `parcel_width`, `parcel_depth`, `parcel_mass`)
-VALUES
-  (1, 'Receiver1', '987654321', '789 Elm St', '67890', 'City3', 'Sender1', '123456789', '123 Maple St', '54321', 'City4',
-   '2023-11-10', '2023-11-12', '2023-11-15',
-   1001, 2001, 0, 1, 2, 10.5, 8.2, 5.0, 2.3),
-  (2, 'Receiver2', '555555555', '456 Pine St', '12345', 'City5', 'Sender2', '111111111', '789 Birch St', '98765', 'City6',
-   '2023-11-11', '2023-11-13', '2023-11-16',
-   1002, 2002, 0, 1, 2, 12.0, 9.8, 6.2, 3.5),
-   (3, 'Receiver3', '556655555', '456 Pine St', '12345', 'City5', 'Sender2', '111111111', '789 Birch St', '98765', 'City6',
-   '2023-11-11', '2023-11-13', '2023-11-16',
-   1003, 2003, 0, 2, 2, 12.0, 9.8, 6.2, 3.5);
+-- -- Insert parcel data
+-- INSERT INTO `parcel`
+--   (`id_user`, `reciever_name`, `reciever_telephone`, `reciever_street_address`, `reciever_postal_code`, `reciever_city`,
+--    `sender_name`, `sender_telephone`, `sender_street_address`, `sender_postal_code`, `sender_city`,
+--    `parcel_dropoff_date`, `parcel_pickup_date`, `parcel_last_pickup_date`,
+--    `parcel_dropoff_code`, `parcel_pickup_code`, `parcel_status`, `parcel_dropoff_locker`, `parcel_pickup_locker`,
+--    `parcel_height`, `parcel_width`, `parcel_depth`, `parcel_mass`)
+-- VALUES
+--   (1, 'Receiver1', '987654321', '789 Elm St', '67890', 'City3', 'Sender1', '123456789', '123 Maple St', '54321', 'City4',
+--    '2023-11-10', '2023-11-12', '2023-11-15',
+--    1001, 2001, 0, 1, 2, 10.5, 8.2, 5.0, 2.3),
+--   (2, 'Receiver2', '555555555', '456 Pine St', '12345', 'City5', 'Sender2', '111111111', '789 Birch St', '98765', 'City6',
+--    '2023-11-11', '2023-11-13', '2023-11-16',
+--    1002, 2002, 0, 1, 2, 12.0, 9.8, 6.2, 3.5),
+--    (3, 'Receiver3', '556655555', '456 Pine St', '12345', 'City5', 'Sender2', '111111111', '789 Birch St', '98765', 'City6',
+--    '2023-11-11', '2023-11-13', '2023-11-16',
+--    1003, 2003, 0, 2, 2, 12.0, 9.8, 6.2, 3.5);
 
--- Insert locker data
-INSERT INTO `locker` (`locker_number`,`id_cabinet`,  `cabinet_status`, `parcel_id`) VALUES
-(1, 1, 2, NULL),
-(1, 2, 2, NULL),
-(1, 3, 2, NULL),
-(1, 4, 2, NULL),
-(1, 5, 1, NULL),
+-- -- Insert locker data
+-- INSERT INTO `locker` (`locker_number`,`id_cabinet`,  `cabinet_status`, `parcel_id`) VALUES
+-- (1, 1, 2, NULL),
+-- (1, 2, 2, NULL),
+-- (1, 3, 2, NULL),
+-- (1, 4, 2, NULL),
+-- (1, 5, 1, NULL),
 
-(2, 1, 1, NULL),
-(2, 2, 1, NULL),
-(2, 3, 1, NULL),
-(2, 4, 1, NULL),
-(2, 5, 1, NULL),
+-- (2, 1, 1, NULL),
+-- (2, 2, 1, NULL),
+-- (2, 3, 1, NULL),
+-- (2, 4, 1, NULL),
+-- (2, 5, 1, NULL),
 
-(3, 1, 1, NULL);
+-- (3, 1, 1, NULL);
 
 
-select * from user;
-select * from parcel;
-select * from locker;
+-- select * from user;
+-- select * from parcel;
+-- select * from locker;
